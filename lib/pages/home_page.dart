a05ab74a7156c0ce8268111d456686f6aad55730
anggunn03 <anggunokt310@gmail.com>
2025-11-19 10:48:23 +0700

    Perbaiki struktur HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  HomePage({super.key});
+  final complaints = [
+    {
+      'judul': 'Jalan Berlubang',
+      'tujuan': 'Dinas Tenaga Kerja',
+      'status': 'Dalam Proses',
+      'tanggal': '2024-06-01',
+    },
+    {
+      'judul': 'Lampu Jalan Mati',
+      'tujuan': 'Dinas Perhubungan',
+      'status': 'Selesai',
+      'tanggal': '2024-05-28',
+    },
+    {
+      'judul': 'Sampah Menumpuk',
+      'tujuan': 'Dinas Kebersihan',
+      'status': 'Dalam Proses',
+      'tanggal': '2024-06-03',
+    },
+  ];
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -13,15 +31,25 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header Section
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft : Radius.circular(20),
+                  bottomRight : Radius.circular(20),
+                ),
+              ),
+              padding: const EdgeInsets.all(16.0),
+            )
+          ],
+        )
+      )
     );
   }
 }
