c7c803be6b9ce54a82c73ce7456f8bb71af79bb4
anggunn03 <anggunokt310@gmail.com>
2025-11-20 09:43:47 +0700

    Update UI HomePage (daftar pengaduan)

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   HomePage({super.key});
   final complaints = [
     {
@@ -24,19 +24,15 @@ class HomePage extends StatefulWidget {
   ];
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
+        child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Header Section
+            // Header 
             Container(
               decoration: const BoxDecoration(
                 color: Colors.blue,
@@ -46,15 +42,18 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-              child: const Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
-                      Image(image:  AssetImage('assets/logo.png'), height : 40,),
-                    
-                      SizedBox(width : 10),
-                      Column(
+                      Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/9/9f/Lambang_Kota_Malang.png',
+                        width : 50,
+                        height : 50,
+                      ),
+                       const SizedBox(width : 10),
+                       const Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
@@ -83,24 +82,69 @@ class _HomePageState extends State<HomePage> {
             const Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
-                'Halo, Selamat Datang!',
+                'Halo, Warga Malang!',
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 16,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
             // Daftar Pengaduan
-            const Padding(
-              padding: EdgeInsets.all(16),
-              child: Text(
-                'Hallo Warga Malang!',
-                style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                children: [
+                  Expanded(
+                    child:Card(
+                      color: Colors.white,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)
+                      ),
+                      child: const Padding(
+                        padding:EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Icon(Icons.mail_outline, color: Color(0xFF1565C0)),
+                            SizedBox(height : 8),
+                            Text('Jumlah Keluhan'),
+                            SizedBox(height : 4),
+                            Text('3', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                            ),
+                          ],
+                        )
+                      ),
+                    )
+                  ),
+                  Expanded(
+                    child: Card(
+                      color: Colors.white,
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            Icon(Icons.check_circle_outline, color: Color(0xFF1565C0)),
+                            SizedBox(height : 8),
+                            Text('Keluhan Selesai'),
+                            SizedBox(height : 4),
+                            Text('1', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)
+                            ),
+                          ],
+                        )
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        )
-      )
+          ]
+        ),  
+      ),
+    ),
     );
   }
 }
