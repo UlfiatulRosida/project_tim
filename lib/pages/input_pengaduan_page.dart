76814172e51d28b0816fd566acc93ff2237dc400
anggunn03 <anggunokt310@gmail.com>
2025-11-27 10:21:23 +0700

    add halaman input pengaduan

---
+++ lib/pages/input_pengaduan_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InputPengaduanPage extends StatelessWidget {
+  const InputPengaduanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Input Pengaduan'),
+      ),
+      body: const Center(
+        child: Text('Halaman Input Pengaduan'),
+      ),
+    );
+  }
+}
