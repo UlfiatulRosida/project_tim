3523ae0b049c4e2b28b201b390fe0ad4d4f5d050
UlfiatulRosida <ulfiarosidah52@gmail.com>
2025-11-27 10:20:07 +0700

    Menambahkan halaman Register dengan controller input, Form key, State untuk show/hide password, State untuk loading, Navigasi kembali ke login

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:project_tim/services/auth_service.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  // input controller
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _obscurePassword = true;
+  bool _isLoading = false;
+
+// Navigasi balik ke login
+  void_goBack() {
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
